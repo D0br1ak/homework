bf5c1628a63b7bcadec32275f47b2b0f20aacfc8
D0br1ak <olezhka_pavlov_01@list.ru>
2025-10-12 15:00:09 +0300

    Add files via upload

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -16,3 +16,28 @@ if (!document.body.classList.contains('body--opened-menu')){
 
   }
 })()
+
+const modalButton = document.querySelector('.company__img-button')
+const modal = document.querySelector('.modal')
+
+
+modalButton.addEventListener('click', openModal)
+modal.addEventListener('click', closeModal)
+
+function openModal(e) {
+
+e.preventDefault()
+
+document.body.classList.toggle('body--opened-modal')
+}
+function closeModal(e){
+
+  e.preventDefault()
+
+  const target = e.target
+  
+  if(target.closest('.modal__close-btn') || target.classList.contains('modal')){
+    document.body.classList.remove('body--opened-modal')
+  }
+
+}
