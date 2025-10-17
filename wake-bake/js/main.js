c7d117c848cecafa3cad0aadfa8c18c57f820df4
D0br1ak <olezhka_pavlov_01@list.ru>
2025-10-17 14:59:44 +0300

    Add files via upload

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -161,4 +161,8 @@
       },
     }
   });
+  // Телефон
+  const telImputs = document.querySelectorAll('input[type="tel"]')
+  const im = new Inputmask('+7 (999) 999-99-99')
+  im.mask(telImputs)
 })()
